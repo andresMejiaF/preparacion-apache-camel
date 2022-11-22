1b6bc63af011c9cbe0c5e46ff45db3189fbbaf8b
Andrés Santiago Mejía Figueroa <andressantiagomejiafigueroa@gmail.com>
2022-11-22 10:53:06 -0500

    Intercambio de objetos

--- camelp1/src/main/java/process/ProcessDataChangeProccesor.java
+++ camelp1/src/main/java/process/ProcessDataChangeProccesor.java
@@ -1,12 +1,25 @@
 package process;
 
+import model.Person;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 
 public class ProcessDataChangeProccesor implements Processor {
     @Override
     public void process(Exchange exchange) throws Exception {
+        /*
         System.out.println("Body In = " + exchange.getIn().getBody());
         System.out.println("Cabecera1 = " + exchange.getIn().getHeader("Cabecera1"));
+         */
+        System.out.println("Body In = ");
+       // Person person =  exchange.getIn().getBody(Person.class); // castea a persona
+        Person person = exchange.getIn().getHeader("person", Person.class);
+        if(person != null){
+            System.out.println("Person name: " + person.getName());
+        }else{
+            System.out.println("Person NUll");
+        }
+
+      //  System.out.println("Cabecera1 = " + exchange.getIn().getHeader("Cabecera1"));
     }
 }
--- camelp1/src/main/java/process/SetDataExchangeProcessor.java
+++ camelp1/src/main/java/process/SetDataExchangeProcessor.java
@@ -1,5 +1,6 @@
 package process;
 
+import model.Person;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 
@@ -7,8 +8,17 @@ public class SetDataExchangeProcessor implements Processor {
 
     @Override
     public void process(Exchange exchange) throws Exception {
+        /*
         System.out.println("Body IN ="+ exchange.getIn().getBody());
         exchange.getOut().setBody("Body definido a partir de process");
         exchange.getOut().setHeader("Cabecera1", "Cabecera establecida en process");
+         */
+        /*
+        Person person = new Person("Jaime", 34);
+        exchange.getOut().setBody(person);
+        */
+        //pasando el objeto en cabecera
+        Person person = new Person("Jaime", 34);
+        exchange.getOut().setHeader("person", person);
     }
 }
