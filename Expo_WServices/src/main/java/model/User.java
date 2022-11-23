d377103dc2906d1cf8d9ec7da08a0233fd3b42a3
Andrés Santiago Mejía Figueroa <andressantiagomejiafigueroa@gmail.com>
2022-11-23 10:51:27 -0500

    Exponiendo web services

---
+++ Expo_WServices/src/main/java/model/User.java
@@ -0,0 +1,60 @@
+
+package model;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.processing.Generated;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "name",
+    "age"
+})
+@Generated("jsonschema2pojo")
+public class User {
+
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("age")
+    private Integer age;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("name")
+    public String getName() {
+        return name;
+    }
+
+    @JsonProperty("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonProperty("age")
+    public Integer getAge() {
+        return age;
+    }
+
+    @JsonProperty("age")
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
