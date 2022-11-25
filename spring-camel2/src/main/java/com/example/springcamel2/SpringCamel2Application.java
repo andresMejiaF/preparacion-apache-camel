0c221fd46e14aaa3934321b7150e3df96631d5bc
Andrés Santiago Mejía Figueroa <andressantiagomejiafigueroa@gmail.com>
2022-11-25 12:45:16 -0500

    Camel + Springboot microservicio2

---
+++ spring-camel2/src/main/java/com/example/springcamel2/SpringCamel2Application.java
@@ -0,0 +1,13 @@
+package com.example.springcamel2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCamel2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCamel2Application.class, args);
+    }
+
+}
---
+++ spring-camel2/src/test/java/com/example/springcamel2/SpringCamel2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcamel2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCamel2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
